95bb8ee6373a2ec2fdfb1d3717b96712a9bc3936
Eniko Illes <illesenikoszabina@gmail.com>
2022-04-09 12:58:15 +0200

    megoldások 1.commit

--- src/main/java/main/Theorem.java
+++ src/main/java/main/Theorem.java
@@ -8,16 +8,24 @@ public class Theorem {
 
     /* Milyen primitív adattípusok léteznek a Java-ban? Sorold fel őket! - 1p
         Ide jön a válaszod...
+        byte,short,int,long,float, double,char,boolean
      */
 
 
     /* Mi a különbség a while és a do-while ciklusok között? - 2p
         Ide jön a válaszod...
+        a while az csak abban az esetben fut le, hogyha teljesül a ()-be irt kikötés.
+        A do while-t akkor használjuk.hogyha minddenképp minimum 1x fusson le
      */
 
 
     /* Mik a tömbök? (Mire használhatóak? Mikor érdemes használni őket? Hogyan hozhatjuk létre őket?) - 3p
         Ide jön a válaszod...
+        a tömbök arra használatosak h azonos tipusu elemeket tároljunk bennük
+        0. indextől számoljuk őket.
+         int[] numbers = {1,2,3,4,5}
+         int[] numbers = new int[5] --> a tömb hosszát jelenti ez a szám
+         String[] names = new String[25];
      */
 
 }
